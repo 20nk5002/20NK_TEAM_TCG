ae8310d1a96e3b21754deead57f452f9b0c1b303
太刀川昌宏 <20nk5008@koudo.ac.jp>
2020-12-01 15:33:48 +0900

    prlyerの更新

---
+++ hoge/player_mesu.cpp
@@ -0,0 +1,45 @@
+#include"DxLib.h"
+#include"player_mesu.h"
+#include"keys.h"
+
+
+
+Player_Mesu::Player_Mesu() 
+{
+    init();
+}
+
+Player_Mesu::~Player_Mesu() 
+{
+    destroy();
+}
+bool Player_Mesu ::init()
+{
+    textur = 0;
+    x_ = x1_ = 0;
+    y_ = y1_ = 0;
+    f_ = 0;
+   
+    if( (textur = LoadGraph( "karichip.png" )) == -1 ) {
+       
+        return false;
+    }
+    return true;
+    }
+void  Player_Mesu::update() 
+{
+    //コントローラーの入力の取得
+    XINPUT_STATE xinput;
+    GetJoypadXInputState( DX_INPUT_PAD1, &xinput );
+    const char* keys[] = Keyboard::getPressed();
+
+      if( Keys[ KEY_INPUT_RIGHT ] == 1 || xinput.Buttons[ XINPUT_BUTTON_DPAD_RIGHT ]
+    }
+void  Player_Mesu::draw()
+{
+
+    }
+void  Player_Mesu::destroy()
+{
+
+    }
--- hoge/player_mesu.h
+++ hoge/player_mesu.h
@@ -1,1 +1,19 @@
 #pragma once
+//プレイアー
+class Player_Mesu
+{
+private:
+    int textur;
+    int x_, y_, f_, x1_, y1_;
+    
+
+
+public:
+    Player_Mesu();
+    ~Player_Mesu();
+    bool init();
+    void update();
+    void draw();
+    void destroy();
+
+};
